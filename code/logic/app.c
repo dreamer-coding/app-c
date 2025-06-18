9392780ffe00ab38cf0dcb898a249225130dc009
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-06-18 11:05:42 -0500

    small changes

--- code/logic/app.c
+++ code/logic/app.c
@@ -13,37 +13,34 @@
  */
 #include "fossil/code/app.h"
 #include <unistd.h>
-#include <string.h>
-#include <stdbool.h>
 
-
-void show_commands(void) {
-    fossil_io_printf("{blue}Usage: fossil_app [OPTIONS]{reset}\n");
+void show_commands(char* app_name) {
+    fossil_io_printf("{blue}Usage: {cyan}%s{blue} <options>{reset}\n", app_name);
     fossil_io_printf("{cyan}  --help           Show this help message{reset}\n");
     fossil_io_printf("{cyan}  --version        Show version information{reset}\n");
     fossil_io_printf("{cyan}  color            Enable colored output ({yellow}'enable'/disable/auto{cyan}){reset}\n");
-    exit(0);
+    exit(FOSSIL_IO_SUCCESS);
 }
 
 void show_version(void) {
     fossil_io_printf("{blue}%s version %s{reset}\n", FOSSIL_APP_NAME, FOSSIL_APP_VERSION);
-    exit(0);
+    exit(FOSSIL_IO_SUCCESS);
 }
 
 bool app_entry(int argc, char** argv) {
-    for (int i = 1; i < argc; ++i) {
-        if (argv[i] == NULL) continue;
-        if (strcmp(argv[i], "--help") == 0) {
-            show_commands();
-        } else if (strcmp(argv[i], "--version") == 0) {
+    for (u32 i = 1; i < argc; ++i) {
+        if (argv[i] == cnullptr) continue;
+        if (fossil_io_cstring_compare(argv[i], "--help") == 0) {
+            show_commands(argv[0]);
+        } else if (fossil_io_cstring_compare(argv[i], "--version") == 0) {
             show_version();
-        } else if (strcmp(argv[i], "color") == 0) {
-            if (i + 1 < argc && argv[i + 1] != NULL) {
-                if (strcmp(argv[i + 1], "enable") == 0) {
+        } else if (fossil_io_cstring_compare(argv[i], "color") == 0) {
+            if (i + 1 < argc && argv[i + 1] != cnullptr) {
+                if (fossil_io_cstring_compare(argv[i + 1], "enable") == 0) {
                     FOSSIL_IO_COLOR_ENABLE = true;
-                } else if (strcmp(argv[i + 1], "disable") == 0) {
+                } else if (fossil_io_cstring_compare(argv[i + 1], "disable") == 0) {
                     FOSSIL_IO_COLOR_ENABLE = false;
-                } else if (strcmp(argv[i + 1], "auto") == 0) {
+                } else if (fossil_io_cstring_compare(argv[i + 1], "auto") == 0) {
                     FOSSIL_IO_COLOR_ENABLE = isatty(STDOUT_FILENO);
                 }
                 ++i; // Skip next argument
